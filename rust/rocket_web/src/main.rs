cf1cbb348c7588f9d987ee5043fe55698299798e
Andrea Manzini <ilmanzo@gmail.com>
2020-10-02 14:51:18 +0200

    set workers and log level

--- rust/rocket_web/src/main.rs
+++ rust/rocket_web/src/main.rs
@@ -3,7 +3,7 @@
 #[macro_use] 
 extern crate rocket;
 
-use rocket::config::{Config,Environment};
+use rocket::config::{Config,Environment,LoggingLevel};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -13,7 +13,9 @@ fn index() -> &'static str {
 fn main() {
     let config = Config::build(Environment::Staging)
     .address("0.0.0.0")
+    .log_level(LoggingLevel::Critical)
     .port(8080)
+    .workers(128)
     .unwrap();
 
     rocket::custom(config).mount("/", routes![index]).launch();
